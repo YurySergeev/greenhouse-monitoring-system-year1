fa43b79920ca339e6cb09879bab0578c41876d89
Ronny Coulson <149505115+ronnycoulson1@users.noreply.github.com>
2026-03-06 01:57:55 -0500

    fix: update transform_data fields to match dashboard schema, add schedule for data collection

--- backend/app/test/apirequest.py
+++ backend/app/test/apirequest.py
@@ -1,6 +1,14 @@
+from datetime import datetime
 import requests
+import json
+import pymongo
+import schedule
+import time
 
+
+#fetch api from openweather API
 def get_weather():
+
     url = "https://api.openweathermap.org/data/2.5/weather"
 
     params = {
@@ -16,12 +24,74 @@ def get_weather():
     if response.status_code == 200:
         data = response.json()
 
-
+        print("weather:", data["weather"][0]["description"])
         print("Temperature:", data["main"]["temp"])
+        print("temp_min:", data["main"]["temp_min"])
+        print("temp_max:", data["main"]["temp_max"])
         print("Humidity:", data["main"]["humidity"])
+        print("City",data["name"])
+        print("Timestamp:", datetime.now())
+        return data
     else:
         print("Error")
+        return None
 
-    print(response.status_code)
-    print(response.json())
+
+#dictionary that has attributes with data that will be saved into mongo
+def transform_data(api_data):
+    record = {
+        "zone" : "zone1",
+        "area" : "upper_plants",
+        "source" : "openweather",
+        "temp_c": api_data["main"]["temp"],
+        "humidity_pct": api_data["main"]["humidity"],
+        "city": api_data["name"],
+        "temp_min": api_data["main"]["temp_min"],
+        "temp_max": api_data["main"]["temp_max"],
+        "weather_desc": api_data["weather"][0]["description"],
+        "ts": datetime.now()
+    }
+
+    return record
+
+
+#save data to mongo
+def save_data(records):
+    #connections
+    MONGO_URI="mongodb+srv://rcoulson_db_user:7q6kDfRuldzW2COa@greenhouse-clouster.n6uuf84.mongodb.net/greenhouse_db?retryWrites=true&w=majority&appName=greenhouse-clouster"
+    client = pymongo.MongoClient(MONGO_URI)
+    db = client["greenhouse-clouster"]
+    collection = db["weather_data"]
+    collection.insert_one(records)
+    print("data saved")
+
+def collect_data_to_db():
+    api_data = get_weather()
+    final_data = transform_data(api_data)
+    save_data(final_data)
+
+
+
+
+
+
+# every 5 minutes, collect weather data,
+# run through get_weather, transform_data, and save_data,
+# then save to MongoDB forever until script is stopped
+schedule.every(30).seconds.do(collect_data_to_db)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
+'''
+ to store data daily automatically 
+ option 1 - leave the terminal running, dont close terminal 
+ option 2 - deploy to cloud server,
+
+ '''
+
+
 
--- backend/app/test/main.py
+++ backend/app/test/main.py
@@ -1,8 +1,15 @@
 from apirequest import get_weather
+from apirequest import transform_data
+from apirequest import save_data
+
 
 def main():
     print("Starting backend test…")
-    get_weather()
+    api_data = get_weather()
+    #transform_data(api_data)
+
+    save_data_result = transform_data(api_data)
+    save_data(save_data_result)
 
 if __name__ == "__main__":
     main()
