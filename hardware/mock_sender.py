0ed8a657a58d959c773ad833c559e33d07482785
Yury Sergeev <60663844+YurySergeev@users.noreply.github.com>
2026-04-27 15:35:58 -0400

    serv

--- hardware/mock_sender.py
+++ hardware/mock_sender.py
@@ -20,7 +20,7 @@ from mock_sensor import MockSensorNode
 # local IP if running on a different device.
 # --------------------------------------------------
 
-SERVER_URL = "http://192.168.1.85:5000/data"
+SERVER_URL = "http://192.168.1.147:5000/data"
 INTERVAL   = 5   # seconds between each reading
 
 
